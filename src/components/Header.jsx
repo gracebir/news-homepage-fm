3d2bdb1a1e86d00958e20734fe18ad276c756113
gracebir <grace.devolop@gmail.com>
2023-09-16 19:41:02 +0200

    header desktop

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { navItems } from '../utils/data'
+import MenuModal from './MenuModal'
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <header className="container mx-auto flex items-center justify-between py-4">
+      <img src={navItems.logo} alt="logo" />
+      <nav className='hidden lg:flex space-x-10'>
+        {navItems.navLinks.map((item)=> (
+          <a className='text-lg text-dark-grayish hover:text-red font-medium' key={item} href="#">
+            {item}
+          </a>
+        ))}
+      </nav>
+      <button onClick={()=> setIsOpen(!isOpen)} className='block lg:hidden z-50'>
+        <img src={isOpen ? navItems.close : navItems.hamburger } alt="menu" />
+      </button>
+      {isOpen && <MenuModal/>}
+    </header>
+  )
+}
+
+export default Header
