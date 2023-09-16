dba29578cb9c4d0a3135767bcb828525d49cc5fc
gracebir <grace.devolop@gmail.com>
2023-09-16 19:53:15 +0200

    sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header"
+import Hero from "./sections/Hero"
+import Spots from "./sections/Spots"
 
 function App() {
   return (
     <div className="min-h-screen font-inter w-full py-4 lg:py-10 px-6 lg:px-0">
       <Header/>
       <div className="container mx-auto">
+        <Hero/>
+        <Spots/>
       </div>
     </div>
   )
