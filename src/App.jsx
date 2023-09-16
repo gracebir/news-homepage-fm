5614773462154840c62e559c54acc0e872bb85c1
gracebir <grace.devolop@gmail.com>
2023-09-16 19:44:40 +0200

    mains view

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Header from "./components/Header"
 
 function App() {
   return (
-    <>
-      <h1>tailwind install</h1>
-    </>
+    <div className="min-h-screen font-inter w-full py-4 lg:py-10 px-6 lg:px-0">
+      <Header/>
+      <div className="container mx-auto">
+      </div>
+    </div>
   )
 }
 
