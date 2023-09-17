c9d62252ebc7e2258b17ab28f844e9930939888a
gracebir <grace.devolop@gmail.com>
2023-09-17 07:40:36 +0200

    animate nav component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { navItems } from '../utils/data'
 import MenuModal from './MenuModal'
+import { AnimatePresence } from 'framer-motion'
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -8,7 +9,7 @@ const Header = () => {
     <header className="py-6 lg:py-10 w-full sticky top-0 bg-off-white z-20">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 lg:px-0">
         <img src={navItems.logo} alt="logo" />
-        <nav className='hidden lg:flex space-x-10'>
+        <nav className='hidden lg:flex space-x-12'>
           {navItems.navLinks.map((item) => (
             <a className='text-lg text-dark-grayish hover:text-red font-medium' key={item} href="#">
               {item}
@@ -19,7 +20,7 @@ const Header = () => {
           <img src={isOpen ? navItems.close : navItems.hamburger} alt="menu" />
         </button>
       </div>
-      {isOpen && <MenuModal />}
+      {isOpen && <AnimatePresence><MenuModal /></AnimatePresence>}
     </header>
   )
 }
--- src/components/MenuModal.jsx
+++ src/components/MenuModal.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import { navItems } from '../utils/data'
+import { motion } from 'framer-motion'
 
 const MenuModal = () => {
   return (
-    <div className='fixed inset-0 flex lg:hidden justify-end h-screen top-0 right-0 bg-black bg-opacity-30 z-30'>
-      <div className='h-full w-4/5 bg-off-white py-40 px-6'>
+    <motion.div
+    initial="hidden"
+    whileInView="show"
+     className='fixed inset-0 flex lg:hidden justify-end h-screen top-0 right-0 bg-black bg-opacity-30 z-30'>
+      <motion.div
+      initial={{
+        x: "100%"
+      }}
+      whileInView={{
+        x: "0"
+      }}
+       className='h-full w-4/5 bg-off-white py-40 px-6'>
         <div className='flex flex-col gap-6'>
           {navItems.navLinks.map((item)=> (
             <a className='font-medium' key={`1-${item}`} href="#">{item}</a>
           ))}
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
 
